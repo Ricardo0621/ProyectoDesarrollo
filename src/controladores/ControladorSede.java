e1209aabaad23363ecdaa867bf77a7830292bd9a
dbueno96 <daniel.bueno@correounivalle.edu.co>
2017-10-25 03:44:43 -0500

    Add files via upload
    Commit de avance para modificar sedes (2)

--- src/controladores/ControladorSede.java
+++ src/controladores/ControladorSede.java
@@ -13,25 +13,88 @@ import logica.Sede;
  * @author diegoruiz
  */
 public class ControladorSede {
-    DaoSede daoSede;
     
-    public ControladorSede(){
-        daoSede = new DaoSede();
+    DaoSede daoSede; 
+    
+    public ControladorSede () {
+        daoSede = new DaoSede(); 
+    }
+ 
+    public String datosVacios (String identificacion, String nombre, String direccion, String telefono)
+    {
+        Sede s= new Sede (identificacion, nombre, direccion, telefono);
+        if (!(s.validarDatosVacios()))
+            return "Todos los campos son obligatorios.";
+        else return "";
+    }
+    public String datosIdentificacion (String identificacion, String nombre, String direccion, String telefono)
+    {
+        Sede s = new Sede (identificacion, nombre, direccion, telefono);
+        if(!(s.validarCaracteresIdentificacion()))
+            return "Identificación con uno o varios caracteres inválidos.";
+        else return "";
+    }
+            
+    public String datosNombre (String identificacion, String nombre, String direccion, String telefono)
+    {
+        Sede s = new Sede (identificacion, nombre, direccion, telefono);
+        if(!(s.validarCaracteresNombre()))
+            return "Nombre con uno o varios caracteres inválidos.";
+        else return "";
     }
     
-    public int  insertarSede(String  identificacion, String nombre, String direccion, String telefono){
-        int result = 0;
-        return result;
-    }//end
-
-    public Sede consultarAsistente(String identificacion){
-        Sede sede = new Sede();
-        return sede;
+    public String datosDireccion (String identificacion, String nombre, String direccion, String telefono)
+    {
+        Sede s = new Sede (identificacion, nombre, direccion,telefono);
+        if(!(s.validarCaracteresNombre()))
+            return "Dirección con uno o varios caracteres inválidos.";
+        else return "";
     }
-
-    public void listarSedes(){}
     
-    public void cerrarConexionBD(){
-        daoSede.cerrarConexionBD();
-    } 
+    public String datosTelefono( String identificacion, String nombre, String direccion, String telefono)
+    {
+        Sede s = new Sede (identificacion, nombre, direccion, telefono);
+        if(!(s.validarCaracteresTelefono()))
+            return"Teléfono con uno o varios caracteres inválidos.";
+        else return "";
+    }
+    
+   
+    public boolean validarDatosModif(String identificacion, String nombre, String direccion, String telefono)
+    {
+        Sede s = new Sede(identificacion, nombre, direccion, telefono);
+        
+        if (!s.validarDatosVacios() )
+            return false; 
+            //return "Todos los campos son obligatorios";
+        else if (!(s.validarCaracteresDireccion()))
+            return false;
+          //  return "Dirección con uno o varios caracteres inválidos.";
+        else if (!(s.validarCaracteresIdentificacion() ))
+            return false;
+         //   return "Identificación con uno o varios caracteres inválidos";
+        else if (!(s.validarCaracteresTelefono()))
+            return false;
+         //   return "Teléfono con uno o varios caracteres inválidos.";
+        else if (!(s.validarCaracteresNombre() ))
+            return false;
+         //   return "Nombre con uno o varios caracteres inválidos.";
+        else //return ""; 
+            return true;
+    }
+    public int  modificarSede(String identificacion, String nombre, String direccion, String telefono)
+    {
+        Sede s = new Sede(identificacion, nombre, direccion, telefono);
+        System.out.println("se va a modificar una sede");
+        
+        
+        
+        int result = daoSede.modificarSede(s); 
+        
+       
+        System.out.println("se modificó una sede");
+        
+        return result; 
+        
+    }
 }
---
+++ src/controladores/DaoSede.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package accesoDatos;
+
+import java.sql.*;
+import logica.Sede;
+
+/**
+ *
+ * @author diegoruiz
+ */
+public class DaoSede {
+    FachadaBD fachada;
+    
+    public DaoSede(){
+        fachada = new FachadaBD();
+    }
+    public int guardarSede(){
+        int filasGuardadas = 0;
+        return filasGuardadas;
+    }
+    public Sede consultarAsistente(){
+        Sede sede = null;
+        return sede;
+    }
+    public int modificarSede(Sede sede ){
+       
+        String sql_modificar;
+        sql_modificar = "UPDATE sede SET nombre = " + sede.getNombre() +","
+                        + "direccion = "+ sede.getDirecion() + ","
+                        + "telefono = "+ sede.getTelefono()  +" ;"
+                        + "WHERE identificacion = "+ sede.getIdentificacion()
+                + ";"; 
+        
+        System.out.println("Se construyó sentencia SQL");
+        try{
+        Connection conn= fachada.conectar();
+        Statement sentencia = conn.createStatement();
+        int numFilas = sentencia.executeUpdate(sql_modificar);
+        conn.close();
+        return numFilas;
+        }
+        catch(SQLException e){ System.out.println(e); }
+        catch(Exception e){ System.out.println(e); }
+        return -1;
+        //fin guardar
+    }
+    public void listarSedes(){}
+    public void borrarSede(String identificacion){}
+    public void cerrarConexionBD(){
+       fachada.closeConection(fachada.getConnetion());
+    }
+    
+}
