cc47101b5ed6dfd98648f9373e2040e811199146
alejo <alejoar76@gmail.com>
2017-10-25 08:35:13 -0500

    cambios git

--- src/accesoDatos/FachadaBD.java
+++ src/accesoDatos/FachadaBD.java
@@ -3,44 +3,44 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package accesoDatos;
-import java.sql.*;
+
 /**
  *
- * @author diegoruiz
+ * @author alejo
  */
+
+
+import java.sql.*;
 public class FachadaBD {
-    String url, usuario, password;
-    Connection conexion =null;
-    Statement instruccion;
-    ResultSet tabla;
-    FachadaBD(){
-        url="jdbc:postgresql://localhost/proyecto_desarrollo";
-        usuario="root";
-        password="123";
-    }
-
-
-
-    public Connection conectar(){
+	String url, usuario, password;
+	Connection conexion = null;
+	Statement instruccion;
+	ResultSet tabla;
+	FachadaBD(){
+	url="jdbc:postgresql://localhost:5432/db_desarrollo";
+        usuario="alejo";
+        password="laniakea";
+	}
+	
+	public Connection conectar(){
         try {
-            // Se carga el driver
-            Class.forName("org.postgresql.Driver");
-            //System.out.println( "Driver Cargado" );
-            } catch( Exception e ) {
-                System.out.println( "No se pudo cargar el driver." );
-            }
+        // Se carga el driver
+        Class.forName("org.postgresql.Driver");
+        //System.out.println( "Driver Cargado" );
+        } catch( Exception e ) {
+            System.out.println( "No se pudo cargar el driver." );
+        }
 
-            try{
-                     //Crear el objeto de conexion a la base de datos
-                     conexion = DriverManager.getConnection(url, usuario, password);
-                     System.out.println( "Conexion Abierta" );
-                     return conexion;
-                  //Crear objeto Statement para realizar queries a la base de datos
-             } catch( Exception e ) {
-                System.out.println( "No se pudo abrir la bd." );
-                return null;
-             }
+        try{
+                 //Crear el objeto de conexion a la base de datos
+                 conexion = DriverManager.getConnection(url, usuario, password);
+                 System.out.println( "Conexion Abierta" );
+                 return conexion;
+              //Crear objeto Statement para realizar queries a la base de datos
+         } catch( Exception e ) {
+            System.out.println( "No se pudo abrir la bd." );
+            return null;
+         }
 
     }//end connectar
 
@@ -51,17 +51,18 @@ public class FachadaBD {
         else{
               return conexion;      
         }
-
+        
     }
-
+    
     public void closeConection(Connection c){
         try{
             if (conexion != null){
                 c.close();
-                System.out.println( "Conexión cerrada" );
             }
+             
         } catch( Exception e ) {
             System.out.println( "No se pudo cerrar." );
         }
     }
+
 }
